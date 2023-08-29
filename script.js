e5217c9a5474554827341aef0268e2e692babd95
nayan_gaikwad <nayan.gaikwad@hbwsl.com>
2023-08-29 09:53:33 +0530

    added hamburger

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById('hamburger').addEventListener('click', () => {
+    let ele = document.getElementById('hamburger');
+    let menu = document.getElementById('navbar__menu');
+    if (ele.classList.contains('clicked')){
+        ele.classList.remove('clicked');
+        ele.src='./assets/hamburger.png';
+        menu.style.display = "none";
+    }
+    else {
+        ele.classList.add('clicked');
+        menu.style.display = "block";
+        ele.src='./assets/close.png';
+        menu.style.height = "12rem";
+    }
+   
+    console.log('hamburger clicked');
+});
